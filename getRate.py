db2207bf007989264065e67439e6bb7414dfb8fb
QingYu2017 <30379078+QingYu2017@users.noreply.github.com>
2018-08-05 21:08:20 +0800

    Create getRate.py

---
+++ getRate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- 
+#! /root/Envs/v36/bin/python
+#Auth:Qing.Yu
+#Mail:1753330141@qq.com
+# Ver:V1.0
+#Date:2018-08-05
+#中国银行汇率，每分钟发布一次，使用requests、bs4库，自定义cls_mssql库（base pymssql）
+
+import requests as r,cls_mssql
+from bs4 import BeautifulSoup as bs
+
+url='http://www.boc.cn/sourcedb/whpj/index.html'
+s=cls_mssql.db_mssql()
+#create table exchange_rate(货币名称 nvarchar(50),现汇买入价 decimal(10,2),现钞买入价 decimal(10,2),现汇卖出价 decimal(10,2),现钞卖出价 decimal(10,2),中行折算价 decimal(10,2),发布日期 date,发布时间 time)
+def fill_blank(str):
+    if str=='':
+        return 'null'
+    else:
+        return str
+
+p=r.get(url)
+tables=bs(p.content, "html.parser").find_all('table')
+trs=tables[1].find_all('tr')
+for i in range(1,len(trs)):
+    tds=trs[i].find_all('td')
+    s.sql_str="insert into exchange_rate values('%s',%s,%s,%s,%s,%s,'%s','%s')"%(tds[0].text,fill_blank(tds[1].text),fill_blank(tds[2].text),fill_blank(tds[3].text),fill_blank(tds[4].text),tds[5].text,tds[6].text,tds[7].text)
+    s.db_exec()
+    print(s.sql_str)
+    #for j in range(len(tds)):
+        #print('货币名称： %s 现汇买入价： %s 现钞买入价： %s 现汇卖出价： %s 现钞卖出价： %s 中行折算价： %s 发布日期： %s 发布时间： %s'%(tds[0],tds[1],tds[2],tds[3],tds[4],tds[5],tds[6],tds[7]))
+        #print('|'.join(tds))
